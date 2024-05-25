8fbc492601e497be8f52fef25f80424c613b85c3
timofey <tim@ongoteam.yaconnect.com>
2024-05-25 19:49:56 +0000

    Dealer

---
+++ src/ready_weak.rs
@@ -0,0 +1,112 @@
+use std::{
+    collections::HashSet,
+    sync::{Arc, Weak},
+    task::{Context, Wake, Waker},
+};
+
+use futures_util::task::AtomicWaker;
+
+use crate::route::Key;
+
+pub(crate) struct Ready<K>(Arc<std::sync::Mutex<HashSet<K>>>);
+
+impl<K> Default for Ready<K> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+pub(crate) struct ReadyWeak<K>(Weak<std::sync::Mutex<HashSet<K>>>);
+
+impl<K> Default for ReadyWeak<K> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+impl<K> Ready<K> {
+    pub(crate) fn lock(&self) -> std::sync::MutexGuard<'_, HashSet<K>> {
+        self.0.lock().unwrap_or_else(|e| e.into_inner())
+    }
+
+    pub(crate) fn downgrade(&self) -> ReadyWeak<K> {
+        ReadyWeak(Arc::downgrade(&self.0))
+    }
+}
+
+impl<K> ReadyWeak<K> {
+    pub(crate) fn lock(&self, f: impl FnOnce(std::sync::MutexGuard<'_, HashSet<K>>)) {
+        if let Some(ready) = self.0.upgrade() {
+            f(ready.lock().unwrap_or_else(|e| e.into_inner()));
+        }
+    }
+}
+
+impl<K: Key> Ready<K> {
+    pub(crate) fn remove(&self, key: &K) {
+        self.lock().remove(key);
+    }
+
+    pub(crate) fn next(&self) -> Option<K> {
+        let mut set = self.lock();
+        let key = set.iter().next()?.clone();
+        set.remove(&key);
+        Some(key)
+    }
+}
+
+impl<K: Key> ReadyWeak<K> {
+    pub(crate) fn insert(&self, key: K) {
+        self.lock(|mut ready| {
+            ready.insert(key);
+        });
+    }
+}
+
+impl<K: Key> Extend<K> for ReadyWeak<K> {
+    fn extend<T: IntoIterator<Item = K>>(&mut self, iter: T) {
+        self.lock(|mut ready| ready.extend(iter))
+    }
+}
+
+pub(crate) struct ConnectionWaker<K> {
+    pub(crate) waker: AtomicWaker,
+    ready: ReadyWeak<K>,
+    key: K,
+}
+
+impl<K> ConnectionWaker<K> {
+    pub(crate) fn new(key: K, ready: ReadyWeak<K>) -> Arc<Self> {
+        Arc::new(Self {
+            waker: Default::default(),
+            ready,
+            key,
+        })
+    }
+}
+
+impl<K: Key> Wake for ConnectionWaker<K> {
+    fn wake(self: Arc<Self>) {
+        self.ready.insert(self.key.clone());
+        self.waker.wake();
+    }
+}
+
+impl<K: Key> ConnectionWaker<K> {
+    pub(crate) fn poll<T>(
+        self: &Arc<Self>,
+        cx: &mut Context<'_>,
+        f: impl FnOnce(&mut Context<'_>) -> T,
+    ) -> T {
+        self.waker.register(cx.waker());
+        f(&mut Context::from_waker(&Waker::from(self.clone())))
+    }
+}
+
+pub(crate) struct Connection<K, S> {
+    pub(crate) stream: S,
+    pub(crate) next: Arc<ConnectionWaker<K>>,
+    pub(crate) ready: Arc<ConnectionWaker<K>>,
+    pub(crate) flush: Arc<ConnectionWaker<K>>,
+    pub(crate) close: Arc<ConnectionWaker<K>>,
+}
