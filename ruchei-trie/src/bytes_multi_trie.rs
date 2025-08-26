b8038b1ec1e4a48980fc33d68efd549dd94a02b0
parrrate <tim@ongoteam.yaconnect.com>
2025-08-26 22:01:53 +0000

    `BytesMultiTrie`

---
+++ ruchei-trie/src/bytes_multi_trie.rs
@@ -0,0 +1,70 @@
+use std::collections::BTreeMap;
+
+use slab::Slab;
+
+use crate::{NodeId, Trie};
+
+pub struct BytesMultiTrie {
+    keys: Trie<BTreeMap<NodeId, usize>>,
+    collections: Trie<Slab<NodeId>>,
+}
+
+impl BytesMultiTrie {
+    pub fn add(&mut self, collection: &[u8], key: &[u8]) {
+        if !self.collections.contains_key(collection) {
+            self.collections.insert(collection, Slab::new());
+        }
+        if self.keys.get(key).is_none() {
+            self.keys.insert(key, BTreeMap::new());
+        }
+        let (collection, slab) = self.collections.get_mut(collection).expect("just inserted");
+        let (id, map) = self.keys.get_mut(key).expect("just inserted");
+        map.entry(collection).or_insert_with(|| slab.insert(id));
+    }
+
+    pub fn remove(&mut self, collection: &[u8], key: &[u8]) {
+        let Some((collection, slab)) = self.collections.get_mut(collection) else {
+            return;
+        };
+        let Some((id, map)) = self.keys.get_mut(key) else {
+            return;
+        };
+        let Some(index) = map.remove(&collection) else {
+            return;
+        };
+        slab.remove(index);
+        if map.is_empty() {
+            self.keys.remove_at(id).expect("invalid state");
+        }
+        if slab.is_empty() {
+            self.collections
+                .remove_at(collection)
+                .expect("invalid state");
+        }
+    }
+
+    pub fn clear(&mut self, collection: &[u8]) {
+        let Some((collection, slab)) = self.collections.remove(collection) else {
+            return;
+        };
+        for (index_expected, id) in slab {
+            let map = self.keys.try_index_mut(id).expect("invalid state");
+            let index_actual = map.remove(&collection).expect("invalid state");
+            assert_eq!(index_actual, index_expected);
+            if map.is_empty() {
+                self.keys.remove_at(id).expect("invalid state");
+            }
+        }
+    }
+
+    pub fn is_empty(&self, collection: &[u8]) -> bool {
+        !self.collections.contains_key(collection)
+    }
+
+    pub fn len(&self, collection: &[u8]) -> usize {
+        self.collections
+            .get(collection)
+            .map(|(_, slab)| slab.len())
+            .unwrap_or_default()
+    }
+}
