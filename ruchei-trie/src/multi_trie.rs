a80e0b3ac8a24ed4ff7b10f824d521faa4eb8107
parrrate <tim@ongoteam.yaconnect.com>
2025-08-27 13:05:11 +0000

    `MultiTriePrefix`

--- ruchei-trie/src/multi_trie.rs
+++ ruchei-trie/src/multi_trie.rs
@@ -1,3 +1,5 @@
+use std::borrow::Borrow;
+
 pub trait MultiTrie<Collection: ?Sized> {
     fn remove(&mut self, collection: &Collection, key: &[u8]);
     fn contains(&self, collection: &Collection, key: &[u8]) -> bool;
@@ -12,4 +14,12 @@ pub trait MultiTrieAddOwned<Collection>: MultiTrie<Collection> {
 
 pub trait MultiTrieAddRef<Collection: ?Sized>: MultiTrie<Collection> {
     fn add_ref(&mut self, collection: &Collection, key: &[u8]);
+}
+
+pub trait MultiTriePrefix: MultiTrie<Self::Collection> {
+    type Collection: ?Sized;
+    fn prefix_collect<'a>(
+        &'a self,
+        suffix: &[u8],
+    ) -> impl 'a + IntoIterator<Item: 'a + Borrow<Self::Collection>>;
 }
