32e79f737a43b136d9f18e120ac321fc091daffb
parrrate <alisa@parrrate.com>
2025-09-12 18:15:55 +0000

    read-many total time

--- examples/bench-read-many.rs
+++ examples/bench-read-many.rs
@@ -1,11 +1,13 @@
 use std::{pin::pin, time::Instant};
 
-use async_std::{stream::StreamExt, task};
+use async_std::{channel::unbounded, stream::StreamExt, task};
 
 #[async_std::main]
 async fn main() {
     let mut tasks = Vec::new();
+    let (started_s, started_r) = unbounded();
     for _ in 0..16 {
+        let started_s = started_s.clone();
         tasks.push(task::spawn(async move {
             let mut stream = pin!(
                 async_tungstenite::async_std::connect_async("ws://127.0.0.1:8080/")
@@ -15,13 +17,16 @@ async fn main() {
             );
             stream.next().await.unwrap().unwrap();
             let start = Instant::now();
+            let _ = started_s.send(start).await;
             for _ in 1..1_000_000 {
                 stream.next().await.unwrap().unwrap();
             }
             println!("{:?}", start.elapsed());
         }));
     }
+    let start = started_r.recv().await.unwrap();
     for task in tasks {
         task.await;
     }
+    println!("{:?} @ total", start.elapsed());
 }
