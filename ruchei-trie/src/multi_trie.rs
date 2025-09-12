b1725e9dc736047cb21d9f8381e358ce0148a645
parrrate <alisa@parrrate.com>
2025-09-12 18:16:21 +0000

    `MultiTrie`

---
+++ ruchei-trie/src/multi_trie.rs
@@ -0,0 +1,57 @@
+use std::collections::BTreeMap;
+
+use slab::Slab;
+
+use crate::{NodeId, Trie};
+
+pub struct MultiTrie<T> {
+    trie: Trie<BTreeMap<T, usize>>,
+    map: BTreeMap<T, Slab<NodeId>>,
+}
+
+impl<T: Clone + Ord> MultiTrie<T> {
+    pub fn add(&mut self, collection: T, key: &[u8]) {
+        if !self.map.contains_key(&collection) {
+            self.map.insert(collection.clone(), Slab::new());
+        }
+        if self.trie.get(key).is_none() {
+            self.trie.insert(key, BTreeMap::new());
+        }
+        let slab = self.map.get_mut(&collection).expect("just inserted");
+        let (id, map) = self.trie.get_mut(key).expect("just inserted");
+        map.entry(collection).or_insert_with(|| slab.insert(id));
+    }
+
+    pub fn remove(&mut self, collection: &T, key: &[u8]) {
+        let Some(slab) = self.map.get_mut(collection) else {
+            return;
+        };
+        let Some((id, map)) = self.trie.get_mut(key) else {
+            return;
+        };
+        let Some(index) = map.remove(collection) else {
+            return;
+        };
+        slab.remove(index);
+        if map.is_empty() {
+            self.trie.remove_at(id).expect("invalid state");
+        }
+        if slab.is_empty() {
+            self.map.remove(collection).expect("invalid state");
+        }
+    }
+
+    pub fn clear(&mut self, collection: &T) {
+        let Some(slab) = self.map.remove(collection) else {
+            return;
+        };
+        for (index_expected, id) in slab {
+            let map = self.trie.try_index_mut(id).expect("invalid state");
+            let index_actual = map.remove(collection).expect("invalid state");
+            assert_eq!(index_actual, index_expected);
+            if map.is_empty() {
+                self.trie.remove_at(id).expect("invalid state");
+            }
+        }
+    }
+}
