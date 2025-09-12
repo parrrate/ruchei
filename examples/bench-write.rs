31e4a92d2ca1873e2e117ce4508dc05ad823b501
parrrate <alisa@parrrate.com>
2025-09-12 18:15:55 +0000

    bench-write example task

--- examples/bench-write.rs
+++ examples/bench-write.rs
@@ -1,29 +1,31 @@
-use std::{pin::pin, time::Instant};
+use std::time::Instant;
 
-use async_std::stream::StreamExt;
-use futures_util::SinkExt;
+use async_std::task;
+use futures_util::{SinkExt, StreamExt};
 
 #[async_std::main]
 async fn main() {
-    let mut stream = pin!(
-        async_tungstenite::async_std::connect_async("ws://127.0.0.1:8080/")
-            .await
-            .unwrap()
-            .0
-    );
-    stream.send("".into()).await.unwrap();
+    let stream = async_tungstenite::async_std::connect_async("ws://127.0.0.1:8080/")
+        .await
+        .unwrap()
+        .0;
+    let (mut writer, mut reader) = stream.split();
+    let task = task::spawn(async move {
+        reader.next().await.unwrap().unwrap();
+        let start = Instant::now();
+        for _ in 1..1_000_000 {
+            reader.next().await.unwrap().unwrap();
+        }
+        println!("{:?}", start.elapsed());
+    });
+    writer.send("0".into()).await.unwrap();
     let start = Instant::now();
-    stream
+    writer
         .send_all(&mut futures_util::stream::iter(
             (1..1_000_000).map(|i| Ok(format!("{i}").into())),
         ))
         .await
         .unwrap();
     println!("{:?}", start.elapsed());
-    stream.next().await.unwrap().unwrap();
-    let start = Instant::now();
-    for _ in 1..1_000_000 {
-        stream.next().await.unwrap().unwrap();
-    }
-    println!("{:?}", start.elapsed());
+    task.await;
 }
