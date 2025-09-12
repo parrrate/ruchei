9f8913b3349823ed2a6921a90c6dfab6c9865a9b
parrrate <alisa@parrrate.com>
2025-09-12 18:16:15 +0000

    route::slab

---
+++ examples/echo-router-slab.rs
@@ -0,0 +1,26 @@
+//! [`ruchei::route`] with [`ruchei::echo::route`]
+
+use async_net::TcpListener;
+use futures_util::StreamExt;
+use ruchei::{
+    concurrent::ConcurrentExt, echo::route::EchoRoute, poll_on_wake::PollOnWakeExt,
+    route::slab::RouterSlabExt,
+};
+
+#[async_std::main]
+async fn main() {
+    TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap()
+        .incoming()
+        .poll_on_wake()
+        .filter_map(|r| async { r.ok() })
+        .map(async_tungstenite::accept_async)
+        .fuse()
+        .concurrent()
+        .filter_map(|r| async { r.ok() })
+        .route_slab(|_| {})
+        .echo_route()
+        .await
+        .unwrap();
+}
