ba285ef3041b639290bfdaaa3a5143c4c6d8c760
parrrate <alisa@parrrate.com>
2025-09-12 18:15:53 +0000

    send-receive example

---
+++ examples/send-receive.rs
@@ -0,0 +1,19 @@
+use std::pin::pin;
+
+use async_std::stream::StreamExt;
+use futures_util::SinkExt;
+
+#[async_std::main]
+async fn main() {
+    let mut stream = pin!(
+        async_tungstenite::async_std::connect_async("ws://127.0.0.1:8080/")
+            .await
+            .unwrap()
+            .0
+    );
+    stream.send("hi".into()).await.unwrap();
+    loop {
+        let msg = stream.next().await.unwrap().unwrap();
+        println!("{msg}");
+    }
+}
