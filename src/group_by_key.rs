6c21e3b2003600a615b42b08b363c72be2f2e307
parrrate <alisa@parrrate.com>
2025-09-12 18:16:00 +0000

    group_by_key docs

--- src/group_by_key.rs
+++ src/group_by_key.rs
@@ -1,3 +1,82 @@
+//! Grouping of items by key.
+//!
+//! Sends new items to a group as long as it is alive. Otherwise, creates a new group.
+//!
+//! ## Example
+//! * First message is treated as a group key.
+//! * All messages within a group are replayed on new connections joining.
+//! * When all participants leave, the history is cleared (by deleting the group).
+//!
+//! ```rust
+//! # mod __ {
+//! # use std::marker::PhantomData;
+//! # use async_std::net::TcpListener;
+//! # use futures_channel::mpsc::{unbounded, UnboundedReceiver, UnboundedSender};
+//! # use futures_util::{future::ready, StreamExt};
+//! # use ruchei::{
+//! #     concurrent::ConcurrentExt,
+//! #     echo::buffered::EchoBuffered,
+//! #     group_by_key::{Group, GroupByKey},
+//! #     multicast::replay::MulticastReplay,
+//! #     poll_on_wake::PollOnWakeExt,
+//! #     timeout_unused::TimeoutUnused,
+//! # };
+//! /* imports omitted */
+//!
+//! struct ChannelGroup<Item>(PhantomData<Item>);
+//!
+//! impl<Item> Group for ChannelGroup<Item> {
+//!     type Item = Item;
+//!
+//!     type Sender = UnboundedSender<Item>;
+//!
+//!     type Receiver = UnboundedReceiver<Item>;
+//!
+//!     fn send(&mut self, sender: &mut Self::Sender, item: Self::Item) {
+//!         let _ = sender.unbounded_send(item);
+//!     }
+//!
+//!     fn pair(&mut self) -> (Self::Sender, Self::Receiver) {
+//!         unbounded()
+//!     }
+//! }
+//!
+//! #[async_std::main]
+//! async fn main() {
+//!     TcpListener::bind("127.0.0.1:8080")
+//!         .await
+//!         .unwrap()
+//!         .incoming()
+//!         .poll_on_wake()
+//!         .filter_map(|r| async { r.ok() })
+//!         .map(|stream| async move {
+//!             let mut stream = async_tungstenite::accept_async(stream).await.ok()?;
+//!             // First message is treated as group key
+//!             let group = stream.next().await?.ok()?;
+//!             Some((group, stream))
+//!         })
+//!         .fuse()
+//!         .concurrent()
+//!         .filter_map(|o| async move {
+//!             o.map(|(group, s)| (group.into_data(), s.poll_on_wake()))
+//!         })
+//!         .group_by_key(ChannelGroup(PhantomData))
+//!         .for_each_concurrent(None, |(receiver, guard)| async move {
+//!             let _guard = guard;
+//!             receiver
+//!                 // When all participants leave, the group is deleted
+//!                 .timeout_unused(|| ready(()))
+//!                 // All messages within a group are replayed on new connection joining
+//!                 .multicast_replay(|_| {})
+//!                 .echo_buffered()
+//!                 .await
+//!                 .unwrap();
+//!         })
+//!         .await;
+//! }
+//! # }
+//! ```
+
 use std::{
     collections::HashMap,
     hash::Hash,
@@ -14,6 +93,7 @@ use futures_util::{
 };
 use pin_project::pin_project;
 
+/// Closes the [`Group`] when [`Drop`]ped.
 #[derive(Debug)]
 pub struct GroupGuard<K>(OwnedMutexGuard<K>);
 
@@ -23,15 +103,20 @@ impl<K> AsRef<K> for GroupGuard<K> {
     }
 }
 
+/// Strategy for grouping items.
 pub trait Group {
     type Item;
 
+    /// [`Grouped`] holds this
     type Sender;
 
+    /// [`Grouped`] yields this
     type Receiver;
 
+    /// [`Grouped`] calls this when the group already exists
     fn send(&mut self, sender: &mut Self::Sender, item: Self::Item);
 
+    /// [`Grouped`] calls this when a new group is created
     fn pair(&mut self) -> (Self::Sender, Self::Receiver);
 }
 
