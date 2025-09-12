80fa34c1e6c16a392578902f630f7fa00c0e8bea
parrrate <alisa@parrrate.com>
2025-09-12 18:16:04 +0000

    try adding docs inter-links

--- src/multicast.rs
+++ src/multicast.rs
@@ -1,13 +1,19 @@
-//! Convert a `Stream` of `Stream+Sink`s into single `Stream+Sink`.
+//! Convert a [`Stream`] of [`Stream`]+[`Sink`]s into single [`Stream`]+[`Sink`].
 //!
 //! ## A Warning
 //!
-//! All multicasting strategies in this module require polling for `next` to make `send` take any effect.
+//! All multicasting strategies in this module require [polling for `next`]
+//! to make [`send`] take any effect.
 //!
 //! Available ways to avoid that:
 //!
 //! * [`ruchei::read_callback`]
 //! * [`ruchei::rw_isolation`]
+//!
+//! [`Stream`]: futures_util::Stream
+//! [`Sink`]: futures_util::Sink
+//! [polling for `next`]: futures_util::Stream::poll_next
+//! [`send`]: futures_util::Sink::start_send
 
 pub mod buffered;
 pub mod bufferless;
