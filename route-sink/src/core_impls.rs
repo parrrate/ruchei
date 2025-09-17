6d713a69b62308b952feb0904fb3396098331dca
parrrate <alisa@parrrate.com>
2025-09-17 20:49:01 +0000

    `route-sink` `&mut` `impl`s

--- route-sink/src/core_impls.rs
+++ route-sink/src/core_impls.rs
@@ -54,3 +54,40 @@ where
         self.get_mut().as_mut().poll_ready_some(cx)
     }
 }
+
+impl<S: ?Sized + Unpin + FlushRoute<Route, Msg>, Route, Msg> FlushRoute<Route, Msg> for &mut S {
+    fn poll_flush_route(
+        mut self: Pin<&mut Self>,
+        route: &Route,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Self::Error>> {
+        Pin::new(&mut **self).poll_flush_route(route, cx)
+    }
+
+    fn poll_close_route(
+        mut self: Pin<&mut Self>,
+        route: &Route,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Self::Error>> {
+        Pin::new(&mut **self).poll_close_route(route, cx)
+    }
+}
+
+impl<S: ?Sized + Unpin + ReadyRoute<Route, Msg>, Route, Msg> ReadyRoute<Route, Msg> for &mut S {
+    fn poll_ready_route(
+        mut self: Pin<&mut Self>,
+        route: &Route,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), Self::Error>> {
+        Pin::new(&mut **self).poll_ready_route(route, cx)
+    }
+}
+
+impl<S: ?Sized + Unpin + ReadySome<Route, Msg>, Route, Msg> ReadySome<Route, Msg> for &mut S {
+    fn poll_ready_some(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<Route, Self::Error>> {
+        Pin::new(&mut **self).poll_ready_some(cx)
+    }
+}
