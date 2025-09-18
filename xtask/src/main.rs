6bd902915d999fedf250796b0dcdaa5426bc9ae9
parrrate <alisa@parrrate.com>
2025-09-18 15:01:19 +0000

    xtask

---
+++ xtask/src/main.rs
@@ -0,0 +1,14 @@
+use std::process::Command;
+
+fn main() {
+    let token = std::env::var("INPUT_TOKEN").unwrap();
+    let ref_name = std::env::var("GITHUB_REF_NAME").unwrap();
+    let package = ref_name.split_once('/').map(|(n, _)| n);
+    let mut cmd = Command::new("cargo");
+    cmd.arg("publish").arg("--dry-run");
+    if let Some(package) = package {
+        cmd.arg("--package").arg(package);
+    }
+    cmd.env("CARGO_REGISTRIES_CRATES_IO_TOKEN", token);
+    assert!(cmd.status().unwrap().success());
+}
