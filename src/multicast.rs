6072cd4c7938601bc43dcb2c90932d8cc5173b0b
parrrate <alisa@parrrate.com>
2025-09-19 13:44:04 +0000

    remove outdated multicast warning

--- src/multicast.rs
+++ src/multicast.rs
@@ -1,19 +1,7 @@
 //! Convert a [`Stream`] of [`Stream`]`+`[`Sink`]s into single [`Stream`]`+`[`Sink`].
 //!
-//! ## A Warning
-//!
-//! All multicasting strategies in this module require [polling for `next`]
-//! to make [`send`] take any effect.
-//!
-//! Available ways to avoid that:
-//!
-//! * [`ruchei::read_callback`]
-//! * [`ruchei::rw_isolation`]
-//!
 //! [`Stream`]: futures_util::Stream
 //! [`Sink`]: futures_util::Sink
-//! [polling for `next`]: futures_util::Stream::poll_next
-//! [`send`]: futures_util::Sink::start_send
 
 pub mod buffered_slab;
 pub mod bufferless_slab;
