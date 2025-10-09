039f17e8c5906a8f8c6b4488f445922034512fff
parrrate <alisa@parrrate.com>
2025-10-09 00:31:45 +0000

    `echo_route` `poll_connection`

--- src/echo/route.rs
+++ src/echo/route.rs
@@ -5,7 +5,7 @@ use std::{
     task::{Context, Poll},
 };
 
-use futures_util::{Future, TryStream};
+use futures_util::{Future, TryStream, ready};
 use pin_project::pin_project;
 use route_sink::ReadyRoute;
 use ruchei_collections::{
@@ -72,6 +72,38 @@ impl<K: Key, T, S> Echo<S, K, T> {
             this.send.downgrade().insert(ix);
         }
     }
+
+    fn poll_connection<E>(
+        mut self: Pin<&mut Self>,
+        ix: SlabKey,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(), E>>
+    where
+        S: ReadyRoute<K, T, Error = E>,
+    {
+        let mut this = self.as_mut().project();
+        while let Some(connection) = this.connections.get_mut(ix) {
+            if connection.msgs.is_empty() {
+                ready!(connection.send.poll(cx, |cx| {
+                    this.router.as_mut().poll_flush_route(&connection.key, cx)
+                }))?;
+                self.as_mut().remove(ix);
+                this = self.as_mut().project();
+            } else {
+                ready!(connection.send.poll(cx, |cx| {
+                    this.router.as_mut().poll_ready_route(&connection.key, cx)
+                }))?;
+                this.router.as_mut().start_send((
+                    connection.key.clone(),
+                    connection
+                        .msgs
+                        .pop_front()
+                        .expect("no first item but not empty?"),
+                ))?;
+            }
+        }
+        Poll::Ready(Ok(()))
+    }
 }
 
 impl<K: Key, T, E, S: TryStream<Ok = (K, T), Error = E> + ReadyRoute<K, T, Error = E>> Future
@@ -90,38 +122,8 @@ impl<K: Key, T, E, S: TryStream<Ok = (K, T), Error = E> + ReadyRoute<K, T, Error
             }
         }
         while let Some(ix) = this.send.as_mut().next::<OP_WAKE_SEND>(this.connections) {
-            while let Some(connection) = this.connections.get_mut(ix) {
-                if connection.msgs.is_empty() {
-                    match connection.send.poll(cx, |cx| {
-                        this.router.as_mut().poll_flush_route(&connection.key, cx)
-                    })? {
-                        Poll::Ready(()) => {
-                            self.as_mut().remove(ix);
-                            this = self.as_mut().project();
-                        }
-                        Poll::Pending => {
-                            break;
-                        }
-                    }
-                } else {
-                    match connection.send.poll(cx, |cx| {
-                        this.router.as_mut().poll_ready_route(&connection.key, cx)
-                    })? {
-                        Poll::Ready(()) => {
-                            this.router.as_mut().start_send((
-                                connection.key.clone(),
-                                connection
-                                    .msgs
-                                    .pop_front()
-                                    .expect("no first item but not empty?"),
-                            ))?;
-                        }
-                        Poll::Pending => {
-                            break;
-                        }
-                    }
-                }
-            }
+            let _: Poll<()> = self.as_mut().poll_connection(ix, cx)?;
+            this = self.as_mut().project();
         }
         Poll::Pending
     }
