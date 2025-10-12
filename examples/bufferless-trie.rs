5db17258f06933caeed2500cecf05d3ab66eeee2
parrrate <alisa@parrrate.com>
2025-10-12 02:45:41 +0000

    `AutoSub` for trie example (did we get too fast?)

--- examples/bufferless-trie.rs
+++ examples/bufferless-trie.rs
@@ -1,7 +1,13 @@
 //! [`ruchei::multicast::bufferless`] with [`ruchei::echo::buffered`]
 
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
 use async_net::TcpListener;
-use futures_util::{SinkExt, StreamExt, TryStreamExt, future::ready};
+use futures_util::{Sink, SinkExt, Stream, StreamExt, TryStream, TryStreamExt, future::ready};
+use pin_project::pin_project;
 use ruchei::{
     concurrent::ConcurrentExt,
     connection_item::ConnectionItemExt,
@@ -9,6 +15,55 @@ use ruchei::{
     multicast::trie::{MulticastTrie, SubRequest},
     poll_on_wake::PollOnWakeExt,
 };
+
+#[pin_project]
+struct AutoSub<S> {
+    #[pin]
+    inner: S,
+    subbed: bool,
+}
+
+impl<S> AutoSub<S> {
+    fn new(inner: S) -> Self {
+        Self {
+            inner,
+            subbed: false,
+        }
+    }
+}
+
+impl<S: TryStream> Stream for AutoSub<S> {
+    type Item = Result<SubRequest<&'static str, S::Ok>, S::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        let this = self.project();
+        if !std::mem::replace(this.subbed, true) {
+            Poll::Ready(Some(Ok(SubRequest::Sub("prefix"))))
+        } else {
+            this.inner.try_poll_next(cx).map_ok(SubRequest::Other)
+        }
+    }
+}
+
+impl<S: Sink<T>, T> Sink<T> for AutoSub<S> {
+    type Error = S::Error;
+
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_ready(cx)
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
+        self.project().inner.start_send(item)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().inner.poll_close(cx)
+    }
+}
 
 #[async_std::main]
 async fn main() {
@@ -22,13 +77,7 @@ async fn main() {
         .fuse()
         .concurrent()
         .filter_map(|r| async { r.ok() })
-        .map(|s| {
-            s.map_ok(|m| {
-                futures_util::stream::iter([SubRequest::Sub("prefix"), SubRequest::Other(m)])
-                    .map(Ok)
-            })
-            .try_flatten()
-        })
+        .map(AutoSub::new)
         .map(|s| s.with(|(_, m)| ready(Ok::<_, async_tungstenite::tungstenite::Error>(m))))
         .multicast_trie()
         .connection_item_ignore()
